8d4145d4229eba8fa36fbc906f9ed5ad129fc482
Constantrock <4jacet@gmail.com>
2016-04-13 15:06:21 -0500

    LAN based communication
    A LAN based communication tool for secure communication and social
    networking

---
+++ Talk Secure.py
@@ -0,0 +1,120 @@
+#I am writing a program for the purpose of encrypting communication
+#between two computers
+
+#I wrote this code over the summer
+#being young and dumb none of this was commented
+#I have forgotten how much of this math works
+#the variable names don't describe what they are
+#I do, however, have access to explanations of the process
+#I will be rewriting this shortly
+
+#http://sergematovic.tripod.com/rsa1.html
+
+#the next step is tow rewrite this with comments
+#it will serve as the foundation of the encryption
+#then I will write the server side code for finding other computers
+#then I will write the client side code for the actual communication
+#I have no estimates of how long this will take
+
+#the general method is to
+#Generate private key
+#generate public key
+#encrypt message
+#decrypt message
+
+
+#Returns True or False if number is prime or not prime
+def isprime(x):
+    #there are no even primes except two
+    if x % 2 == 0:
+        return False
+    #the median factor is always the square root
+    #there shouldn't be any need to check for even numbers
+    #so start with an odd then increment by two
+    for i in xrange(3, int((x**(1.0/2.0)))+1 , 2):
+        if x % i == 0:
+            return False
+    return True
+"""Generate two large primes"""
+""" Prime1 and Prime2"""
+
+"""n = Prime1 * Prime2"""
+"""z = ((Prime1 - 1) * (Prime2 - 1))"""
+"""Generate Prime3"""
+"""generate a number k that is larger than Prime3 and coprime with Prime3"""
+"""The server's public key is n, k"""
+
+"""This is the server side library"""
+"""it has a list of large primes already"""
+
+
+def PublicKeyGen(Prime1, Prime2, Prime3,):
+    n = Prime1 * Prime2
+    z = ((Prime1 - 1) * (Prime2))
+    while True:
+        k = random.randint(300000 * z, 1000000 * z)
+        if k % z == 0:
+            break
+    return z, k
+
+
+#this magic return the Greatest common denominator
+#I don't remember how
+#I hope it isn't important
+def gcd(a, b):
+    s = 0
+    old_s = 1
+    r = b
+    old_r = a
+    t = 1
+    old_t = 0
+    while r != 0:
+        quotient = old_r / r
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+    return old_r
+
+
+# modular inverse returns s instead of r
+#the modular inverse is the number k so that J * k  = 1 mod(n)
+def modinverse(a, b):
+    s = 0
+    old_s = 1
+    r = b
+    old_r = a
+    t = 1
+    old_t = 0
+    while r != 0:
+        quotient = old_r / r
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+    return old_s
+
+#The authority returns the inverse of e such that it is co prime with p and q?4
+def authority(P, Q, E):
+    if gcd(E, ((P-1) * (Q-1))) != 1:
+        return -1
+    for einverse in xrange(2, ((P-1) * (Q-1)), 1):
+        if (E * einverse) % ((P-1) * (Q-1)) == 1:
+            return einverse
+    return -1
+
+#the modular exponent returns modular exponent
+def modexp(Base, Exponent, Modulus):
+    if Modulus == 1:
+        return 0
+    c = 1
+    #this method is faster although it takes more operations
+    for i in xrange(1, Exponent+1, 1):
+        c = (c * base) % Modulus
+    return c
+
+
+def openrsa(M, E, PQ):
+    if m > PQ:
+        return -1
+    c = modexp(M, E, PQ)
+    return c
+
